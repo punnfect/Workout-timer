1d664a2a2156ef101234bc48e3b75e202af1c124
punnfect <sp.kool@yahoo.com>
2025-11-19 14:32:07 -0600

    Initial Commit

---
+++ src/main/java/com/punnfect/workouttimer/WorkoutTimerApplication.java
@@ -0,0 +1,13 @@
+package com.punnfect.workouttimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutTimerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/punnfect/workouttimer/WorkoutTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punnfect.workouttimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
